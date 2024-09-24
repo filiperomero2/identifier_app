329aa0e721e89259e6d60374a3842192b36e310e
Filipe Moreira <filiperomero2@gmail.com>
2024-09-24 19:56:27 -0300

    Update parse_blast_results.py

--- scripts/parse_blast_results.py
+++ scripts/parse_blast_results.py
@@ -17,6 +17,10 @@ def get_args():
     help='Complete path for the input blast results in tabular format.',
     required = True)
 
+    parser.add_argument('--query', 
+    help='Complete path for the query fasta file.',
+    required = True)
+
     parser.add_argument('--db', 
     help='Complete path for the blast db fasta file.',
     required = True)
@@ -34,7 +38,7 @@ def get_args():
     if os.path.isfile(args['input']) and os.path.isfile(args['db']):
         return args
 
-def read_database(db):
+def read_database(db,query):
     data = {}
     with open(db,"r") as f:
         for line in f.readlines():
@@ -44,12 +48,22 @@ def read_database(db):
                 data[header] = ""
             else:
                 data[header] += line
+    with open(query,"r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith(">"):
+                header = (line.lstrip(">"))
+                data[header] = ""
+            else:
+                data[header] += line
     return data
 
 def parse_output(args):
-    data = read_database(args['db'])
-    df = pd.read_csv(args['input'],header=None,sep="\t")
-    queries = list(df[0].unique())
+    data = read_database(args['db'],args['query'])
+    #df = pd.read_csv(args['input'],header=None,sep="\t")
+    df = pd.read_csv(args['input'],sep="\t")
+    #queries = list(df[0].unique())
+    queries = list(df['qseqid'].unique())
 
     outdir = '/'.join(args['output'].split('/')[:-1])
     if os.path.isdir(outdir) is False:
@@ -58,7 +72,8 @@ def parse_output(args):
     report = open(args['output'],"w")
 
     for query in queries:
-        hits = list(df[1][df[0] == query].unique())  
+        #hits = list(df[1][df[0] == query].unique())
+        hits = list(df['sseqid'][df['qseqid'] == query].unique())  
         if len(hits) >= args['target']:
             my_hits = hits[:args['target']]
         else:
@@ -66,10 +81,7 @@ def parse_output(args):
 
         outfile_fasta = f"{outdir}/top_hits_{query}.fasta"
         with open(outfile_fasta,"w") as out:
-            if query in data.keys():
-                out.write(f">{query}_QUERY\n{data[query]}\n")
-            else:
-                print("Query NOT available in the reference file...")
+            out.write(f">{query}_QUERY\n{data[query]}\n")
             for hit in my_hits:
                 if hit in data.keys():
                     out.write(f">{hit}\n{data[hit]}\n")
